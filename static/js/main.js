2bdc71cddfe2a3de6241e441638f29fe3956586a
LWJGL <78892259+A7e3@users.noreply.github.com>
2024-12-11 18:19:46 -0800

    Create main.js

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+function toClipboard(input) {
+    var textarea = document.createElement("textarea");
+    textarea.textContent = input;
+    textarea.style.position = "fixed"; // Prevent scrolling to bottom of page in MS Edge.
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy"); 
+  
+    document.body.removeChild(textarea);
+}
