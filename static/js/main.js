53e0c97f8bb6dd994a484a503cb3b65e976a2931
LWJGL <78892259+A7e3@users.noreply.github.com>
2024-12-19 14:53:23 -0800

    Fixed!

--- static/js/main.js
+++ static/js/main.js
@@ -13,3 +13,25 @@ function toClipboard(input) {
 $(document).tooltip({
     selector: '[data-toggle="tooltip"]'
 });
+
+fetch('https://qsite.kohihq.net/proxy/getplayers')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); 
+    })
+    .then(responses => {
+        const playersContainer = document.getElementById('players');
+        
+        let htmlContent = `
+            <div class="main-top-content content-center">
+                There are currently ${responses.content} players online
+            </div>
+        `;
+        
+        playersContainer.innerHTML = htmlContent;
+    })
+    .catch(error => {
+        console.error('Error fetching the players:', error);
+    });
